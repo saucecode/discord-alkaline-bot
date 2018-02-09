f81f6ef8e7df1a892c3236ba54dc268c84e89f88
Julian <cahill.julian@gmail.com>
2018-02-10 09:44:53 +1300

    reactions.py placeholder created

---
+++ modules/sxde/reactions.py
@@ -0,0 +1,21 @@
+import discord
+
+class Reactions:
+
+	def __init__(self, client):
+		self.client = client
+
+		self.name = 'Reactions'
+		self.version = '0.1'
+		self.author = 'Julian'
+
+	async def on_message(self, message):
+		if message.content[:2] == self.client.COMMAND_PREFIX * 2:
+			await message.channel.send('reaction detected: %s' % message.content[2:])
+
+	async def on_command(self, message, command, args):
+		if command == 'reactionadd':
+			await message.channel.send('I\'m trapped in a Google data center send help')
+
+plugins = [Reactions]
+commands = ['reactionadd']
