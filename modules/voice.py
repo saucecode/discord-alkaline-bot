1ebd546924457fa613c4dd481984672d78975e81
Julian <cahill.julian@gmail.com>
2018-02-12 11:55:20 +1300

    added playlist commands pladd, plpop, plplay, playlists, plview, and shuffle to voice plugin

--- modules/voice.py
+++ modules/voice.py
@@ -1,5 +1,5 @@
 from .alkalineplugin import AlkalinePlugin
-import discord, asyncio, youtube_dl, requests, urllib3, io, subprocess, threading, json, re, os
+import discord, asyncio, youtube_dl, requests, urllib3, io, subprocess, threading, json, re, os, random
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -26,6 +26,9 @@ class VoiceManager(AlkalinePlugin):
 		self.executor = ThreadPoolExecutor(4)
 		self.http = urllib3.PoolManager()
 
+		self.playlists = {}
+		self.load_playlists()
+
 		self.audio_format_code = '171'
 
 		self.name = 'VoiceManager'
@@ -39,6 +42,17 @@ class VoiceManager(AlkalinePlugin):
 
 		task = self.client.loop.create_task(self.background_task())
 		task.alkaline_identifier = self.name
+
+	def load_playlists(self):
+		if not os.path.exists('data/playlists.json'):
+			self.save_playlists()
+
+		with open('data/playlists.json', 'r') as f:
+			self.playlists = json.load(f)
+
+	def save_playlists(self):
+		with open('data/playlists.json', 'w') as f:
+			json.dump(self.playlists, f, indent=4, separators=(',', ': '))
 
 	async def on_command(self, message: discord.Message, command : str, args : str):
 		if command == 'voice':
@@ -106,6 +120,69 @@ class VoiceManager(AlkalinePlugin):
 				else:
 					await message.channel.send('Removed {} from queue'.format(self.queue.pop()))
 
+		elif command == 'shuffle':
+			random.shuffle(self.queue)
+			await message.channel.send('Queue shuffled.')
+
+		# PLAYLIST COMMANDS
+		elif command == 'pladd':
+			if len(args.split(' ')) == 1:
+				await message.channel.send('Usage: {}{} {}'.format(self.client.COMMAND_PREFIX, command, commands[command]['usage']))
+			else:
+				playlist_name = args.split(' ')[0]
+				query = args[len(playlist_name)+1:]
+
+				if not playlist_name in self.playlists:
+					self.playlists[playlist_name] = []
+
+				search = self.search_songs(query)
+
+				if len(search) > 5:
+					await message.channel.send('Found more than 5 results - please be more specific.')
+				elif len(search) > 1:
+					await message.channel.send('Found these results - please be more specific.\n```\n{}\n```'.format( '\n'.join(search) ))
+				elif len(search) == 0:
+					await message.channel.send('Couldn\'t find a file matching that query.'.format( args ))
+				else:
+					self.playlists[playlist_name].append( search[0] )
+					self.save_playlists()
+					await message.channel.send('Added `{}`. Playlist `{}` now has {} elements.'.format( search[0],playlist_name,len(self.playlists[playlist_name]) ))
+
+		elif command == 'playlists':
+			actual_playlists = [playlist_name for playlist_name in self.playlists if len(self.playlists[playlist_name]) > 0]
+			await message.channel.send('I found {} playlists:\n{}'.format( len(actual_playlists), ' '.join(actual_playlists) ))
+
+		elif command == 'plview':
+			if len(args) == 0 or ' ' in args or not args in self.playlists:
+				await message.channel.send('Please specify playlist name.')
+			else:
+				await message.channel.send('```\n{}\n```'.format( '\n'.join( ['{}. {}'.format(index,song) for index,song in enumerate(self.playlists[args])] ) ))
+
+		elif command == 'plpop':
+			args = args.split(' ')
+			if not len(args) == 2:
+				await message.channel.send('Usage: {}{} {}'.format(self.client.COMMAND_PREFIX, command, commands[command]['usage']))
+			elif not args[0] in self.playlists:
+				await message.channel.send('Specify an existing playlist name.')
+			else:
+				try:
+					int(args[1])
+					item = self.playlists[args[0]].pop(int(args[1]))
+					await message.channel.send('Removed `{}` from playlist `{}`.'.format(item, args[0]))
+					self.save_playlists()
+				except (IndexError, ValueError):
+					await message.channel.send('Usage: {}{} {}'.format(self.client.COMMAND_PREFIX, command, commands[command]['usage']))
+					return
+
+		elif command == 'plplay':
+			if not args in self.playlists:
+				await message.channel.send('Specify an existing playlist name.')
+			else:
+				for fname in self.playlists[args]:
+					self.queue.append( {'type':'file', 'filename':'downloaded/{}'.format(fname)} )
+				await message.channel.send('Added {} new files to the queue.'.format(len(self.playlists[args])))
+
+
 	def search_songs(self, query):
 		files = os.listdir('downloaded/')
 		flatten = lambda l: [item for sublist in l for item in sublist]
@@ -125,7 +202,11 @@ class VoiceManager(AlkalinePlugin):
 				if tag.lower() in f.lower().replace('-',''):
 					count += 1
 			files_sorted.append( (count, f) )
-		files_sorted = [x[1] for x in sorted(files_sorted, reverse=True, key=lambda x:x[0]) if x[0]>1]
+
+		temp_sort = sorted(files_sorted, reverse=True, key=lambda x:x[0])
+		if len(temp_sort) > 1 and temp_sort[0][0] > temp_sort[1][0]:
+			return [temp_sort[0][1]]
+		files_sorted = [x[1] for x in temp_sort if x[0]>1]
 		return files_sorted
 
 	def get_youtube_info(self, url):
@@ -240,5 +321,16 @@ commands = {
 	},
 	'stop':{
 		'desc':  'Stops playing and clears the queue.'
-	}
+	},
+
+	'pladd': {
+		'usage': '[playlist name] [song name]'
+	},
+	'plpop': {
+		'usage': '[playlist name] [song position]'
+	},
+	'plplay': {},
+	'playlists': {},
+	'plview':{},
+	'shuffle':{}
 }
