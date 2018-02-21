dadbc6d7b17d1a487ee6812928bcdc85f825652d
Julian <cahill.julian@gmail.com>
2018-02-21 20:04:31 +1300

    start caching response data for 10 minutes (default). switch to aiohttp to make the requests

--- modules/sxde/reddit.py
+++ modules/sxde/reddit.py
@@ -1,5 +1,5 @@
 from ..alkalineplugin import AlkalinePlugin
-import random, requests
+import random, requests, aiohttp, time
 
 from ..sailortalk import sailor_word
 
@@ -8,6 +8,9 @@ class Reddit(AlkalinePlugin):
 	def __init__(self, client):
 		self.client = client
 
+		self.cached = {}
+		self.expiry_time = 10*60 # 10 minutes
+
 		self.name = 'Reddit'
 		self.version = '0.1'
 		self.author = 'Julian'
@@ -15,15 +18,35 @@ class Reddit(AlkalinePlugin):
 	async def on_command(self, message, command, args):
 		if command == 'rr':
 			subreddit = args.split(' ')[0]
-			dat = requests.get('https://reddit.com/r/' + subreddit + '/.json', headers={'User-Agent':'Discord-Alkaline-Bot'}).json()
-			urls = [i['data']['url'] for i in dat['data']['children']] # pull urls from reddit post list
-			await message.channel.send(random.choice(urls))
+			#dat = requests.get('https://reddit.com/r/' + subreddit + '/.json', headers={'User-Agent':'Discord-Alkaline-Bot'}).json()
+			#urls = [i['data']['url'] for i in dat['data']['children']] # pull urls from reddit post list
+			urls = await self.request_reddit_data('https://reddit.com/r/' + subreddit + '/.json')
+			self.rr_message = await message.channel.send(random.choice(urls))
 
 		if command == 'rrtop':
 			subreddit = args.split(' ')[0]
-			dat = requests.get('https://reddit.com/r/' + subreddit + '/top/.json?t=all', headers={'User-Agent':'Discord-Alkaline-Bot'}).json()
-			urls = [i['data']['url'] for i in dat['data']['children']] # pull urls from reddit post list
+			#dat = requests.get('https://reddit.com/r/' + subreddit + '/top/.json?t=all', headers={'User-Agent':'Discord-Alkaline-Bot'}).json()
+			#urls = [i['data']['url'] for i in dat['data']['children']] # pull urls from reddit post list
+			urls = await self.request_reddit_data('https://reddit.com/r/' + subreddit + '/.json')
 			await message.channel.send(random.choice(urls))
+
+	async def request_reddit_data(self, url):
+		if url in self.cached:
+			now = time.time()
+			if not now > self.cached[url]['expiry']:
+				print(url,'is in the cache')
+				return self.cached[url]['content']
+		else:
+			self.cached[url] = {}
+
+		print('Downloading', url)
+
+		async with aiohttp.ClientSession() as session:
+			async with session.get(url, headers={'User-Agent':'Discord-Alkaline-Bot'}) as resp:
+				dat = await resp.json()
+				self.cached[url]['content'] = [i['data']['url'] for i in dat['data']['children']]
+				self.cached[url]['expiry'] = time.time() + self.expiry_time
+				return self.cached[url]['content']
 
 plugins = [Reddit]
 commands = {
