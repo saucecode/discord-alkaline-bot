1d8931a28c2429b5ee0b867a3f6b9ec43babb28e
Julian <cahill.julian@gmail.com>
2018-06-22 16:34:56 +1200

    port essentials to the new format - I'm not a fan.

--- modules/essentials.py
+++ modules/essentials.py
@@ -38,73 +38,65 @@ class Essentials(AlkalinePlugin):
 		self.version = '0.1'
 		self.author = 'Julian'
 
-	async def on_command(self, message, command, args):
-		if command == 'ping':
-			await message.channel.send('Pong')
+	async def ping(self, message, args):
+		await message.channel.send('Pong')
 
+	async def flip(self, message, args):
+		random_int = random.randint(0,1)
+		if random_int == 0:
+			await message.channel.send('Tails')
+		else:
+			await message.channel.send('Heads')
 
-		elif command == 'flip':
-			random_int = random.randint(0,1)
-			if random_int == 0:
-				await message.channel.send('Tails')
-			else:
-				await message.channel.send('Heads')
+	async def whoami(self, message, args):
+		user = message.author
+		await message.channel.send('Name: %s; Display Name: %s; Discriminator: %s; ID: %i; Server ID: %i' % (user.name, user.display_name, user.discriminator, user.id, message.guild.id))
 
-
-		elif command == 'whoami':
-			user = message.author
+	async def whois(self, message, args):
+		target = args
+		user = discord.utils.find(lambda m: target.lower() in m.name.lower() or target.lower() in m.display_name.lower(), message.guild.members)
+		if user:
 			await message.channel.send('Name: %s; Display Name: %s; Discriminator: %s; ID: %i; Server ID: %i' % (user.name, user.display_name, user.discriminator, user.id, message.guild.id))
+		else:
+			await message.channel.send('Couldn\'t find that user.')
 
+	async def whereami(self, message, args):
+		await message.channel.send('Channel ID: %i' % message.channel.id)
 
-		elif command == 'whois':
-			target = args
-			user = discord.utils.find(lambda m: target.lower() in m.name.lower() or target.lower() in m.display_name.lower(), message.guild.members)
-			if user:
-				await message.channel.send('Name: %s; Display Name: %s; Discriminator: %s; ID: %i; Server ID: %i' % (user.name, user.display_name, user.discriminator, user.id, message.guild.id))
-			else:
-				await message.channel.send('Couldn\'t find that user.')
+	async def define(self, message, args):
+		word = args.split(' ')[0]
+		defs = dictionary.get_definitions(word)[:3]
+		if len(defs) == 0:
+			await message.channel.send('What even is a %s?' % word)
+		else:
+			i=1
+			out = ''
+			for s in defs:
+				out += '%i. %s\n' % (i,s)
+				i += 1
+			await message.channel.send('```%s```' % (out,))
 
+	async def urbandictionary(self, message, args):
+		word = args
+		definition = dictionary.get_urban_definitions(word)[0]
+		for i in range(len(definition['definition'])//2000 + 1):
+			await message.channel.send('```%s```' % definition['definition'][i*2000:i*2000+2000])
+		await message.channel.send('```examples: %s```' % definition['example'])
 
-		elif command == 'whereami':
-			await message.channel.send('Channel ID: %i' % message.channel.id)
+	async def filth(self, message, args):
+		await message.channel.send(sailor_word())
 
+	async def get_perms(self, message, args):
+		await message.channel.send('You have permissions: `%s`' % ' '.join(self.client.get_members_permissions(message.author.id)))
 
-		elif command == 'define':
-			word = args.split(' ')[0]
-			defs = dictionary.get_definitions(word)[:3]
-			if len(defs) == 0:
-				await message.channel.send('What even is a %s?' % word)
-			else:
-				i=1
-				out = ''
-				for s in defs:
-					out += '%i. %s\n' % (i,s)
-					i += 1
-				await message.channel.send('```%s```' % (out,))
+	async def get_ctime(self, message, args):
+		if args and is_float(args):
+			await message.channel.send(time.ctime(float(args)))
 
-
-		elif command == 'ud':
-			word = args
-			definition = dictionary.get_urban_definitions(word)[0]
-			for i in range(len(definition['definition'])//2000 + 1):
-				await message.channel.send('```%s```' % definition['definition'][i*2000:i*2000+2000])
-			await message.channel.send('```examples: %s```' % definition['example'])
-
-
-		elif command == 'filth':
-			await message.channel.send(sailor_word())
-
-		elif command == 'perms':
-			await message.channel.send('You have permissions: `%s`' % ' '.join(self.client.get_members_permissions(message.author.id)))
-
-		elif command == 'ctime':
-			if args and is_float(args):
-				await message.channel.send(time.ctime(float(args)))
-
-		elif command == 'chavatar':
-			if not args.startswith('http'): return
-			avatar_url = args
-			await self.client.user.edit(avatar=requests.get(avatar_url).content)
+	async def chavatar(self, message, args):
+		if not args.startswith('http'): return
+		avatar_url = args
+		await self.client.user.edit(avatar=requests.get(avatar_url).content)
 
 
 class EssentialsCalc(AlkalinePlugin):
@@ -125,44 +117,61 @@ class EssentialsCalc(AlkalinePlugin):
 				await self.answer_message.edit(content=postfix.outputResult( postfix.doPostfix(after.content[1 + 4 + len(self.client.COMMAND_PREFIX):].strip()) ))
 
 	async def on_command(self, message, command, args):
-		if command == 'calc':
-			self.query_message = message
-			self.answer_message = await message.channel.send( postfix.outputResult( postfix.doPostfix(message.content[1 + 4 + len(self.client.COMMAND_PREFIX):].strip()) ) )
+		pass
+
+	async def calc(self, message, args):
+		self.query_message = message
+		self.answer_message = await message.channel.send( postfix.outputResult( postfix.doPostfix(message.content[1 + 4 + len(self.client.COMMAND_PREFIX):].strip()) ) )
 
 plugins = [Essentials, EssentialsCalc]
 commands = {
-	'ping':{},
-	'flip':{},
-	'whoami':{},
-	'whois':{},
+	'ping':{
+		'function': Essentials.ping
+	},
+	'flip':{
+		'function': Essentials.flip
+	},
+	'whoami':{
+		'function': Essentials.whoami
+	},
+	'whois':{
+		'function': Essentials.whois
+	},
 	'define':{
 		'usage': '[word]',
-		'desc':  'Look up the definition of a word.'
+		'desc':  'Look up the definition of a word.',
+		'function': Essentials.define
 	},
 	'ud':{
 		'usage': '[word or phrase]',
-		'desc':  'Look up the definition of a word from urban dictionary.'
+		'desc':  'Look up the definition of a word from urban dictionary.',
+		'function': Essentials.urbandictionary
 	},
-	'whereami':{'perms':['op']},
+	'whereami':{'perms':['op'], 'function': Essentials.whereami},
 	'filth':{
-		'desc': 'Generate a random filthy/offensive word or phrase.'
+		'desc': 'Generate a random filthy/offensive word or phrase.',
+		'function': Essentials.filth
 	},
 	'perms':{
-		'desc': 'Prints your permissions.'
+		'desc': 'Prints your permissions.',
+		'function': Essentials.get_perms
 	},
 	'ctime':{
 		'desc': 'Converts unix epoch number to a human-readable time string.',
-		'example': '1518765410.543'
+		'example': '1518765410.543',
+		'function': Essentials.get_ctime
 	},
 	'chavatar':{
 		'desc': 'Changes the avatar of the bot to the image at url.',
 		'usage': '[url]',
-		'perms': ['op', 'admin']
+		'perms': ['op', 'admin'],
+		'function': Essentials.chavatar
 	},
 
 	'calc':{
 		'usage': '[postfix expr]',
 		'desc':  'Does a calculation.',
 		'example': '3 3 * 4 4 * + sqrt',
+		'function': EssentialsCalc.calc
 	}
 }
